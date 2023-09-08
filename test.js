f8c46010823d401b409533d3513388cf2c4d34b9
Yevhenii <yevmot01@easv365.dk>
2023-09-08 09:58:12 +0200

    second test

--- test.js
+++ test.js
@@ -16,7 +16,14 @@ test("Create new todo", async t => {
 });
 
 test("Mark as done", async t => {
+    await t
     // Create a pre-assertion that validates that no existing completed tasks are on the list.
+    .expect(Selector("ul > li.todo.completed").count).eql(0)
     // Write a test yourself that creates a new task, marks it as completed.
+    .typeText(Selector(".new-todo"), "Completed task")
+    // Act
+    .pressKey("enter")
+    .click(Selector("#comleted"))
     // Assert that the number of completed tasks is now 1.
+    .expect(Selector("ul > li.todo.completed").count).eql(1);
 });
