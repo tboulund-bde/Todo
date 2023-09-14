13e7646889d714e0f30334334968d51c10d49aae
andkoehlert <118164444+andkoehlert@users.noreply.github.com>
2023-09-14 13:05:11 +0200

    Again

--- test.js
+++ test.js
@@ -1,22 +1,28 @@
+// testcafe chrome test.js //
+
 import { Selector } from "testcafe"
 
 fixture`Demo`
     .page("./index.html");
 
-test("Create new todo", async t => {
-    await t
-        // Pre-assertion
-        .expect(Selector("ul.todo-list li.todo").count).eql(0)
-        // Arrange
-        .typeText(Selector(".new-todo"), "Water the flowers")
-        // Act
-        .pressKey("enter")
-        // Assert
-        .expect(Selector("ul.todo-list li.todo").count).eql(1);
-});
 
-test("Mark as done", async t => {
+
+test("Create a new todo", async t => {
+    await t
+    .expect(Selector(".todo-count").innerText).contains("0")        // Arrange
+    .typeText(Selector(".new-todo"), "Water the flowers again agian")
+    // Act
+    .pressKey("enter")
+    .click(Selector(".toggle")) // Close parenthesis here
+
+    .expect(Selector("todo-count").count).eql(0);
+
     // Create a pre-assertion that validates that no existing completed tasks are on the list.
+
     // Write a test yourself that creates a new task, marks it as completed.
     // Assert that the number of completed tasks is now 1.
 });
+
+
+
+
