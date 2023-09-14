9f4946dbecdd6f1a78d36b105e0c168de1ac2af5
Laura <72190730+HYUUMII@users.noreply.github.com>
2023-09-14 20:51:29 +0200

    update test.js
    Wrote a new test.

--- test.js
+++ test.js
@@ -15,8 +15,20 @@ test("Create new todo", async t => {
         .expect(Selector("ul.todo-list li.todo").count).eql(1);
 });
 
-test("Mark as done", async t => {
+    test("Mark as done", async t => {
+        // Create a pre-assertion that validates that no existing completed tasks are on the list.
+        await t
+            .expect(Selector("ul.todo-list li.completed").count).eql(0); 
+    
+        // Write a test yourself that creates a new task, marks it as completed.
+        await t
+            .typeText(Selector(".new-todo"), "Walk the dog")
+            .pressKey("enter")
+            .click(Selector("input.toggle"))
+            .expect(Selector("ul.todo-list li.completed").count).eql(1); // Assert that the number of completed tasks is now 1.
+    });
+
     // Create a pre-assertion that validates that no existing completed tasks are on the list.
     // Write a test yourself that creates a new task, marks it as completed.
     // Assert that the number of completed tasks is now 1.
-});
+
