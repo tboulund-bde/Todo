aba66867a2935f530bd603c65a5dd52121b41d14
kris299b <kripag01@easv365.dk>
2023-09-20 18:18:45 +0200

    Mark as done

--- test.js
+++ test.js
@@ -1,22 +1,29 @@
 import { Selector } from "testcafe"
 
+// fixture`Demo`
+//     .page("./index.html");
+
+// test("Create new todo", async t => {
+//     await t
+//         // Pre-assertion
+//         .expect(Selector("ul.todo-list li.todo").count).eql(0)
+//         // Arrange
+//         .typeText(Selector(".new-todo"), "Water the flowers")
+//         // Act
+//         .pressKey("enter")
+//         // Assert
+//         .expect(Selector("ul.todo-list li.todo").count).eql(1);
+// });
+
 fixture`Demo`
     .page("./index.html");
 
-test("Create new todo", async t => {
-    await t
-        // Pre-assertion
-        .expect(Selector("ul.todo-list li.todo").count).eql(0)
-        // Arrange
-        .typeText(Selector(".new-todo"), "Water the flowers")
-        // Act
-        .pressKey("enter")
-        // Assert
-        .expect(Selector("ul.todo-list li.todo").count).eql(1);
-});
-
 test("Mark as done", async t => {
-    // Create a pre-assertion that validates that no existing completed tasks are on the list.
-    // Write a test yourself that creates a new task, marks it as completed.
-    // Assert that the number of completed tasks is now 1.
+
+    await t
+        .expect(Selector("ul.todo-list li.todo").count).eql(0)
+        .typeText(Selector(".new-todo"), "It works")
+        .pressKey("enter")
+        .click(".toggle")
+        .expect(Selector("ul.todo-list li.todo").count).eql(1);
 });
