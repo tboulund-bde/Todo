26ec09509cfb70c6430959e88aeeb11759b4e9a1
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2023-10-02 00:08:03 +0200

    assignment3

--- test.js
+++ test.js
@@ -3,32 +3,31 @@ import { Selector } from "testcafe"
 fixture`Demo`
     .page("./index.html");
 
-test("Create new todo", async t => {
-    await t
-        // Pre-assertion
-        .expect(Selector("ul.todo-list li.todo").count).eql(0)
-        // Arrange
-        .typeText(Selector(".new-todo"), "Water the flowers")
-        // Act
-        .pressKey("enter")
-        // Assert
-        .expect(Selector("ul.todo-list li.todo").count).eql(1);
-});
+// test("Create new todo", async t => {
+//     await t
+//         // Pre-assertion
+//         .expect(Selector("ul.todo-list li.todo").count).eql(0)
+//         // Arrange
+//         .typeText(Selector(".new-todo"), "Water the flowers")
+//         // Act
+//         .pressKey("enter")
+//         // Assert
+//         .expect(Selector("ul.todo-list li.todo").count).eql(1);
+// });
 
 test("Mark as done", async t => {
     await t
     // Create a pre-assertion that validates that no existing completed tasks are on the list.
+    // Pre-assertion
     .expect(Selector("ul.todo-list li.todo").count).eql(0)
     
     // Write a test yourself that creates a new task, marks it as completed.
-    
     // Arrange
-    .typeText(Selector(".new-todo"), "Eat some more icecream")
-    
+    .typeText(Selector("body > section > header > input"), "Eat some more icecream")
+    // Act
     .pressKey("enter")
-
     .click(Selector("body > section > section > ul > li > div > input"))
-    
+
     // Assert that the number of completed tasks is now 1.
     // Assert
     .expect(Selector("ul.todo-list li.todo").count).eql(1);
