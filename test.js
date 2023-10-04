fd1a6685e47a94298c5c358fde0949ed4e9daa2b
jako3501 <jako3501@easv365.dk>
2023-10-04 23:36:42 +0200

    Test for 'mark as completed'

--- test.js
+++ test.js
@@ -18,6 +18,17 @@ test("Create new todo", async t => {
 });
 
 test("Mark as done", async t => {
+    await t
+        // Pre-assertion
+        .expect(Selector("ul.todo-list li.todo div.view input.toggle:checked").count).eql(0)
+        // Arrange
+        .toggleAttribute(Selector("ul.todo-list li.todo div.view input.toggle"), "checked")
+        // Act
+        .pressKey("enter")
+        // Assert 
+        .expect(Selector("ul.todo-list li.todo div.view input.toggle:checked").count).eql(1);
+
+
     // Create a pre-assertion that validates that no existing completed tasks are on the list.
     // Write a test yourself that creates a new task, marks it as completed.
     // Assert that the number of completed tasks is now 1.
